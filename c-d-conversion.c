4c2ffaaf510bc6d1bbb2a14bbbd5569acf251458
sanjai-pachiappan <sandyysanjai@gmail.com>
2026-04-21 16:32:17 +0530

    c real example
    This folder contains 50 basic C programs written to master the fundamentals of C programming. 
    Suitable for beginners and includes examples for all basic concepts.
    
    Includes:
    • Simple print programs  
    • Arithmetic operations  
    • Decision-making programs  
    • Loop-based programs  
    • Pattern printing  
    • Functions and arrays  
    
    Great for revision and interview preparation.

---
+++ c-d-conversion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  int num1 = 5;
+  int num2 = 2;
+  float sum = (float) num1 / num2;
+
+  printf("%.1f", sum);
+  return 0;
+}
---
+++ c-variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int studentID = 15;
+  int studentAge = 20;
+  float studentFloat = 75.25;
+  char studentGrade = 'B';
+
+
+  printf("Student id :%d\n",studentID );
+  printf("Student id :%d\n",studentAge );
+  printf("Student id :%f\n",studentFloat );
+  printf("Student id :%c",studentGrade);
+  return 0 ;
+}
